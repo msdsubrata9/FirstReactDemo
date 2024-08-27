f4f1f26126a14ffba162312580fc26406ffe1b87
subrata-saha <subrata.saha@sessionai.com>
2024-08-27 19:28:23 +0530

    FEAT: Learned about React Component

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading" }, "Hello World From React!");
+const Title = () => (
+    <div>
+        <h1 className="title">Namaste React using JSX</h1>
+    </div>
+)
 
-console.log(heading);
+const TitleComponent = () => (
+    <div>
+        <h1 className="title">Title Component using React For Assignment</h1>
+    </div>
+)
 
+const HeadingComponent = () => (
+    <div id="container">
+        <Title />
+        {<TitleComponent />}
+        {<TitleComponent></TitleComponent>}
+        <h1 className="heading">
+            Namaste React Functional Component
+        </h1>
+    </div>
+)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent />);
