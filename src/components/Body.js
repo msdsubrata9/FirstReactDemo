27fba7847522b78d3c2b50e3227c39df6203bee9
subrata-saha <subrata.saha@sessionai.com>
2024-09-07 16:14:44 +0530

    FEAT: Add dynamic data fetching with useEffect, search functionality, and login/logout support

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,47 @@
 import RestaurentCard from "./RestaurentCard";
-import { useState } from "react";
-import resList from "../utils/mockData";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
+
 const Body = () => {
-    const [listOfRestaurant, setListOfRestaurant] = useState(resList);
-    return (
+    const [listOfRestaurant, setListOfRestaurant] = useState([]);
+    const [searchText, setSearchText] = useState("");
+    const [filteredListOfRestaurant, setFilteredListOfRestaurant] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch("https://corsproxy.io/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D28.6616862%26lng%3D77.2304635%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING");
+
+        const json = await data.json();
+
+        console.log(json);
+
+        setListOfRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredListOfRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+    return listOfRestaurant.length === 0 ? <Shimmer /> : (
         <div className="body">
             <div className="filter">
+                <div className="search">
+                    <input type="text" className="search-box" value={searchText} onChange={(e) => {
+                        setSearchText(e.target.value);
+                    }} />
+                    <button onClick={() => {
+                        console.log(searchText);
+                        const filteredRestaurant = listOfRestaurant.filter((restaurant) => {
+                            return restaurant.info.name.toLowerCase().includes(searchText.toLowerCase());
+                        })
+                        setFilteredListOfRestaurant(filteredRestaurant);
+                    }}>Search</button>
+                </div>
                 <button className="filter-btn"
                     onClick={() => {
                         const filterlogic = listOfRestaurant.filter((res) => {
                             return res.info.avgRating > 4.3;
                         });
-                        setListOfRestaurant(filterlogic);
+                        setFilteredListOfRestaurant(filterlogic);
                     }}
                 >
                     Top Rated Restaurants
@@ -19,7 +49,7 @@ const Body = () => {
             </div>
             <div className="restaurant-container">
                 {
-                    listOfRestaurant.map((restaurant) => (<RestaurentCard key={restaurant.info.id} resData={restaurant} />))
+                    filteredListOfRestaurant.map((restaurant) => (<RestaurentCard key={restaurant.info.id} resData={restaurant} />))
                 }
             </div>
         </div>
--- src/components/RestaurentCard.js
+++ src/components/RestaurentCard.js
@@ -2,17 +2,19 @@ import { CDN_URL } from "../utils/constants";
 const RestaurentCard = (props) => {
     const { resData } = props;
 
+    const { cloudinaryImageId, name, cuisines, avgRating, costForTwo, sla } = resData?.info;
+
     return (
         <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
             <img
                 className="res-logo"
                 alt="res-logo"
-                src={CDN_URL + resData.info.cloudinaryImageId}></img>
-            <h3>{resData.info.name}</h3>
-            <h4>{resData.info.cuisines.join(", ")}</h4>
-            <h4>{resData.info.avgRating} stars</h4>
-            <h4>{resData.info.costForTwo}</h4>
-            <h4>{resData.info.sla.deliveryTime} minutes</h4>
+                src={CDN_URL + cloudinaryImageId}></img>
+            <h3>{name}</h3>
+            <h4>{cuisines.join(", ")}</h4>
+            <h4>{avgRating} stars</h4>
+            <h4>{costForTwo}</h4>
+            <h4>{sla?.deliveryTime} minutes</h4>
         </div>
     )
 }
